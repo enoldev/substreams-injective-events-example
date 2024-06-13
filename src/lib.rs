85f00e90b36f60709c671bf67d88445facfbb478
Enol Álvarez <enol826@gmail.com>
2024-06-13 15:26:26 +0200

    Update console log

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use crate::pb::cosmos::v1::MyEventList;
 
 #[substreams::handlers::map]
 pub fn map_events(event_list: EventList) -> Result<MyEventList, Error> {
-    substreams::log::println(event_list.events.len().to_string());
+    substreams::log::info!(event_list.events.len().to_string());
 
     let mut my_event_list: Vec<MyEvent> = Vec::new();
 
